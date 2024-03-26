b801ceddb88e30b6f54de368f87d34f15bd5c3d6
euandecoup <euandecoup@gmail.com>
2024-03-26 14:36:05 +0000

    get comment + get comment by id added

--- app/server/database/comment_database.py
+++ app/server/database/comment_database.py
@@ -16,3 +16,16 @@ def comment_helper(comment):
         'user_id': str(comment['user_id']),
         'votes': int(comment['votes'])
     }
+
+#Fetch all comments
+async def retrieve_comments():
+    comments = []
+    async for comment in comment_col.find():
+        comments.append(comment_helper(comment))
+    return comments
+
+#Fetch comment by ID
+async def retrieve_comment(id : str):
+    comment = await comment_col.find_one({'_id' : ObjectId(id)})
+    if comment:
+        return comment_helper(comment)
--- app/server/routes/comment_routes.py
+++ app/server/routes/comment_routes.py
@@ -2,3 +2,29 @@ from fastapi import APIRouter, Body
 
 from fastapi.encoders import jsonable_encoder
 
+from server.database.comment_database import (
+    retrieve_comments,
+    retrieve_comment
+) 
+
+from server.models.comment_models import (
+    ErrorResponseModel,
+    ResponseModel,
+    CommentSchema
+)
+
+router = APIRouter()
+
+@router.get('/', response_description='Users recieved')
+async def get_comments():
+    comments = await retrieve_comments()
+    if comments:
+        return ResponseModel(comments, 'Comments data retrieved successfully')
+    return ResponseModel(comments, 'Returned an empty list')
+
+@router.get('/{id}', response_description='Topic data retrieved successfully')
+async def get_comment_by_id(id):
+    comment = await retrieve_comment(id)
+    if comment:
+        return ResponseModel(comment, 'Comment data retrieved successfully')
+    return ErrorResponseModel('An error occurred.', 404, 'Comment does not exist')
